349d0d09aad6c3d8aeefe3088ebbdf813bb337c1
Elif Çinkılıç <elifcinkilic97@hotmail.com>
2020-03-21 22:20:40 +0300

    role deleted

--- BluSchool/Controllers/HomeController.cs
+++ BluSchool/Controllers/HomeController.cs
@@ -39,68 +39,7 @@ namespace BluSchool.Controllers
             return View();
         }
 
-        [Authorize(Roles = "Admin")]
-        public IActionResult UserList()
-        {
-            var userList = _ctx.Users.ToList();
-            return View(userList);
-        }
-        
-        [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> MakeTeacher(string userId)
-        {
-            var user = _ctx.Users.Where(u => u.Id == userId).FirstOrDefault();
-            var result1 = await _userManager.AddToRoleAsync(user, "Teacher");
-            return View();
-        }
-
-        [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> MakeStudent(string userId)
-        {
-            var user = _ctx.Users.Where(u => u.Id == userId).FirstOrDefault();
-            var result1 = await _userManager.AddToRoleAsync(user, "Student");
-            return View();
-        }
-
-
-        [HttpGet]
-        public async Task<JsonResult> initRole()
-        {
-            bool x = await _roleManager.RoleExistsAsync("Admin");
-            if (!x)
-            {
-                // first we create Admin rool    
-                var role = new IdentityRole();
-                role.Name = "Admin";
-                await _roleManager.CreateAsync(role);
-
-                var teacherRole = new IdentityRole();
-                role.Name = "Teacher";
-                await _roleManager.CreateAsync(teacherRole);
-
-                var studentRole = new IdentityRole();
-                role.Name = "Student";
-                await _roleManager.CreateAsync(studentRole);
-                //Here we create a Admin super user who will maintain the website                   
-
-                var user = new IdentityUser();
-                user.UserName = "admin@admin.com";
-                user.Email = "admin@admin.com";
-
-                string userPWD = "asdasdasD1.";
-
-                IdentityResult chkUser = await _userManager.CreateAsync(user, userPWD);
-
-                //Add default User to Role Admin    
-                if (chkUser.Succeeded)
-                {
-                    var result1 = await _userManager.AddToRoleAsync(user, "Admin");
-                }
-               
-            }
-            return Json(1);
-        }
-
+       
         public async Task<IActionResult> Logout()
         {
             await _signInManager.SignOutAsync();
